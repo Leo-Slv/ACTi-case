ff3bb26c3b2617fa08fd05ffc5df5692e71931e2
Leo-Slv <leonardo1692004@gmail.com>
2025-08-22 20:13:50 -0300

    Https hello-word

---
+++ api-dotnet/api-dotnet/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_dotnet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        // GET: api/hello
+        [HttpGet]
+        public IActionResult SayHello()
+        {
+            return Ok("Hello World from .NET 9!");
+        }
+    }
+}
